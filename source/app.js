e4ac302af622735743c8fc06d4dccc93b2f4ff37
김휘겸 <breakstorm00@gmail.com>
2020-02-23 23:38:18 +0900

    Update webpack and webpack config

---
+++ source/app.js
@@ -0,0 +1,4 @@
+import hello from './hello.js';
+import world from './world.js';
+
+document.querySelector('#app').innerHTML = hello + ' ' + world;
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: "./source/app.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: 'index_bundle.js'
+    }
+}
