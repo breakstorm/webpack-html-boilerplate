1d80eda323aa095230a57c77d11fa5f934ff59df
김휘겸 <breakstorm00@gmail.com>
2020-03-02 12:48:20 +0900

    Add html-webpack-plugin files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -6,5 +7,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "public"),
         filename: 'index_bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'test webpack',
+            filename: 'test.html',
+            template: 'source/template.html'
+        }),
+    ]
 }
